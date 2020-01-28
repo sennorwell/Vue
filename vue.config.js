6cb3ef5a29c18908d686f892facfd465b11f40be
dongsen <1415222054@qq.com>
2020-01-28 22:16:27 +0800

    'ds'

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ module.exports = {
     devServer: {
         /* 跨域的解决办法 */
         proxy: 'https://module-script-tests-gkecnwbwkb.now.sh/no-cors',
-    }
+    },
+    productionSourceMap: false, // 关闭生产环境下的map
 };
