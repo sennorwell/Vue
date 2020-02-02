0659e9a0675bcf91200cbf2ab6a307a2d538456f
dongsen <1415222054@qq.com>
2020-02-02 09:50:01 +0800

    'vue'

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,16 @@
+const path = require('path');
 module.exports = {
     productionSourceMap: false, // 关闭生产环境下的map
     publicPath: '/', // 设置统一的url地址, BASE_URL == publicPath
     devServer: {
-        /* 跨域的解决办法 */
-        proxy: 'https://module-script-tests-gkecnwbwkb.now.sh/no-cors',
-    },
-    pwa: {
-        name: 'My App',
-        themeColor: '#4DBA87',
-        msTileColor: '#000000',
-        appleMobileWebAppCapable: 'yes',
-        appleMobileWebAppStatusBarStyle: 'black',
-
-        // 配置 workbox 插件
-        workboxPluginMode: 'InjectManifest',
-        workboxOptions: {
-            // InjectManifest 模式下 swSrc 是必填的。
-            swSrc: 'dev/sw.js',
-            // ...其它 Workbox 选项...
+        contentBase: path.join(__dirname, 'output.js'),
+        compress: true,
+        port: 9000,
+        open: true,
+        overlay: { // 显示遮罩层 warnings and errors
+            warnings: true,
+            errors: true
         }
-    }
+    },
+    assetsDir: 'assets', //build的静态文件目录
 };
